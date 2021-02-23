919074d1f9806316a77007e2fc343643fc7b8e3f
Tice Cosmos <tice@139.com>
2021-02-23 18:38:03 +0800

    chore: Fix function signature mismatch when link for wasm32-unknown-unknown

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 pub mod configuration;
 mod format_text;
 
-#[cfg(feature = "wasm")]
-#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+#[cfg(all(target_arch = "wasm32", target_os = "unknown", feature = "wasm"))]
 mod wasm_plugin;
 
-#[cfg(feature = "wasm")]
-#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+#[cfg(all(target_arch = "wasm32", target_os = "unknown", feature = "wasm"))]
 pub use wasm_plugin::*;
